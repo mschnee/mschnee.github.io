241a678200cc80efe9fcfc4e530d59d924b088db
= <matthew.schnee@gmail.com>
2014-02-08 19:09:14 -0800

    Mostly working form.  Function, not pretty

--- js/xiv-crafting-calculator.js
+++ js/xiv-crafting-calculator.js
@@ -1,4 +1,4 @@
-(function(w, d)) {
+(function(w, d){
 	var levelBlocks = [
 		// 0
 		   0, 
@@ -17,6 +17,199 @@
 	
 	var init = function() {
 		var content = d.createElement("div");
+		currentLevelInput = d.querySelector("input[name='currentLevel']");
+		targetLevelInput = d.querySelector("input[name='targetLevel']");
+		currentExpInput = d.querySelector("input[name='currentExp']");
+		expPerLeveInput = d.querySelector("input[name='expPerLeve']");
+
+		expNeededOutput = d.querySelector("#resultXp");
+		turninsOutput = d.querySelector("#leveCount");
+		hqTurninsOutput = d.querySelector("#leveHqCount");
+		initBindings();
+
+		updateCalculations();
+	};
+
+	var initBindings = function() {
+		currentLevelInput.addEventListener("keydown", currentLevelKeyDown, true);
+		targetLevelInput.addEventListener("keydown", targetLevelKeyDown, true);
+		currentExpInput.addEventListener("keydown", currentExpKeyDown, true);
+		expPerLeveInput.addEventListener("keydown", currentExpPerLeveKeyDown, true);
 	}
+
+	var currentLevelKeyDown = function(event) {
+		event.stopPropagation();
+		var constraintResult = numericInputConstraint(currentLevelInput, event);
+		if ( constraintResult === true ){
+			setTimeout(updateCalculations,1);
+			return true;
+		} else if (constraintResult === false ) {
+			event.preventDefault();
+			return constraintResult;
+		}
+
+		event.preventDefault();
+		currentLevelInput.value = constraintResult;
+
+		if( targetLevelInput.value <= constraintResult ) {
+			targetLevelInput.value = constraintResult < targetLevelInput.getAttribute("max") ? 
+				parseInt(constraintResult, 10)+1 : cons
+		}
+		updateCalculations();
+		return false;
+	}
+
+	var targetLevelKeyDown = function(event) {
+		event.stopPropagation();
+		var constraintResult = numericInputConstraint(targetLevelInput, event);
+		if ( constraintResult === true ){
+			setTimeout(updateCalculations,1);
+			return true;
+		} else if (constraintResult === false ) {
+			event.preventDefault();
+			return constraintResult;
+		}
+
+		event.preventDefault();
+		targetLevelInput.value = constraintResult;
+		updateCalculations();
+		return false;
+	}
+
+	var currentExpKeyDown = function(event) {
+		event.stopPropagation();
+		var constraintResult = numericInputConstraint(currentExpInput, event);
+		if ( constraintResult === true ){
+			setTimeout(updateCalculations,1);
+			return true;
+		} else if (constraintResult === false ) {
+			event.preventDefault();
+			return constraintResult;
+		}
+
+		event.preventDefault();
+		currentExpInput.value = constraintResult;
+		updateCalculations();
+		return false;
+	}
+
+	var currentExpPerLeveKeyDown = function(event) {
+		event.stopPropagation();
+		var constraintResult = numericInputConstraint(expPerLeveInput, event);
+		if ( constraintResult === true ){
+			setTimeout(updateCalculations,1);
+			return true;
+		} else if (constraintResult === false ) {
+			event.preventDefault();
+			return constraintResult;
+		}
+
+		event.preventDefault();
+		expPerLeveInput.value = constraintResult;
+		updateCalculations();
+		return false;
+	}
+
+	var updateCurrentXpConstraint = function() {
+		currentExpInput.setAttribute("max", 
+			levelBlocks[targetLevelInput.value]
+		);
+		if (currentExpInput.value > levelBlocks[targetLevelInput.value]) {
+			currentExpInput.value = 0;
+		}
+	}
+
+	var updateCalculations = function() {
+		
+		var currentLevel = parseInt(currentLevelInput.value, 10);
+		var targetLevel = parseInt(targetLevelInput.value, 10);
+		var expPerLeve = parseInt(expPerLeveInput.value, 10);
+
+		if( targetLevel <= currentLevel) {
+			expNeededOutput.innerHTML = "0";
+			return;
+		}
+		if ( targetLevel > levelBlocks.length ) {
+			return;
+		}
+		if( !expPerLeve || isNaN(expPerLeve) ) {
+			turninsOutput.innerHTML = "...";
+			hqTurninsOutput.innerHTML = "...";
+			return;
+		}
+		var neededXp = levelBlocks[currentLevel] - currentExpInput.value;
+		for (var l = currentLevel+1; l <= targetLevel && l < levelBlocks.length; l++) {
+			neededXp += levelBlocks[l];
+		}
+		expNeededOutput.innerHTML = neededXp;
+
+		
+		var levesNecessary = neededXp / expPerLeve;
+		turninsOutput.innerHTML = Math.max( Math.ceil(levesNecessary), 1);
+		hqTurninsOutput.innerHTML = Math.max(Math.ceil(levesNecessary / 2), 1)
+
+	}
+
+	/**
+	 *	Metafunction.
+	 *	Will return true, false, or a new string value
+	 */
+	var numericInputConstraint = function(element, event) {
+		switch (event.which) {
+			case 8:
+			case 9:
+			case 13:
+			case 27:
+			case 33:
+			case 34:
+			case 35:
+			case 36:
+			case 37:
+			case 39:
+			case 45:
+			case 46:
+			return true;
+		}
+		var newValInt;
+		var curValStr = element.value;
+		var min = parseInt(element.getAttribute("min"), 10); 
+		var max = parseInt(element.getAttribute("max"), 10);
+		if (event.which == 38) {
+			newValInt = curValStr? parseInt(curValStr, 10)+1: 1;
+		} else if (event.which == 40) {
+			newValInt = curValStr? parseInt(curValStr, 10)-1: 1;
+		} else {
+			var strVal = String.fromCharCode(event.which);
+			
+			if ( ! /^\d/.test(strVal)) {
+				return false;
+			}
+
+			// get the caret position, min attribute and max attribute
+			var caretPos = element.selectionStart || 0;
+			
+
+			// assemble a new value string
+			var newValStr = curValStr.substr(0, caretPos) + strVal + curValStr.substr(caretPos);
+			if (newValStr.substr(0, 1) === "0") {
+				return false;
+			}
+
+			newValInt = parseInt(newValStr, 10);	
+		}
+		
+		if(element.hasAttribute("max")) {
+			if (newValInt >= min && newValInt <= max) {
+				return newValInt;
+			}
+		} else {
+			if (newValInt >= min) {
+				return newValInt;
+			}
+		}
+		return false;
+	}
+
 	d.addEventListener("DOMContentLoaded", init);
-} (window, document)
+})(window, document)
+
